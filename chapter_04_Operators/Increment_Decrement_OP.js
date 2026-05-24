c736efb7fdb2f1724d7a5ff47fa94d19a782ef5a
JapinderKaur7 <104529597+JapinderKaur7@users.noreply.github.com>
2026-05-25 01:04:44 +0530

    Add Increment_Decrement_OP.js and update chapter 04

--- chapter_04_Operators/Increment_Decrement_OP.js
+++ chapter_04_Operators/Increment_Decrement_OP.js
@@ -1,21 +1,36 @@
-let a = 10;
-let b = a++;
-console.log(a);  //11
-console.log(b);  //10
+// let a = 10;
+// let b = a++;
+// console.log(a);  //11
+// console.log(b);  //10
 
 
-let  x= 20;
-let y = ++x;
-console.log(x);  //21
-console.log(y);  //21
+// let  x= 20;
+// let y = ++x;
+// console.log(x);  //21
+// console.log(y);  //21
 
-let n = 100;
-console.log(++n + n);   //202
+// let n = 100;
+// console.log(++n + n);   //202
 
-let s = 200;
-//console.log(s++); //200
-//console.log(++s); //202
-console.log(s++ + ++s); //406   //402
+// let s = 200;
+// //console.log(s++); //200
+// //console.log(++s); //202
+// console.log(s++ + ++s); //406   //402
 
-let l = 300;
-console.log(++l + ++l); //603
+// let l = 300;
+// console.log(++l + ++l); //603
+
+
+
+// let a = 5; 
+// console.log(a++ + ++a)   //5 +7 =12
+// console.log(a); //7
+// console.log(--a + a--) //6 + 6
+// console.log(a); //5
+// console.log(++a); //6
+// console.log(a); //6
+
+
+let a = 5; 
+console.log(a++ + ++a - --a + a-- + ++a); 
+console.log(a);
